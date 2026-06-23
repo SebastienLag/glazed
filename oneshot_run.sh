e5fba57a7056e0e44049baa3060f4430b6cea62f
Jeremiah Blanchard <jjb@bmail.club>
2026-06-23 14:08:15 +0900

    Minor updates to fix build

--- oneshot_run.sh
+++ oneshot_run.sh
@@ -1,8 +1,4 @@
 #! /usr/bin/env bash
-curl -O https://raw.githubusercontent.com/Gairolld/astem-combined/main/compose.yaml
 curl -O https://raw.githubusercontent.com/Gairolld/astem-combined/main/.env
-curl -L -O https://raw.githubusercontent.com/Gairolld/astem-combined/main/DatabaseNearFinal.sql
-curl -L -o ASTEM_DB https://raw.githubusercontent.com/Gairolld/astem-combined/main/db-interface/ASTEM_DB
-chmod +x ASTEM_DB
-docker-compose down -v
-docker-compose up -d
+sudo docker compose down -v
+sudo docker compose up -d
